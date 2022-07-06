0185e49902679943775868270c64a5145c13c69c
leandriel <leandro88valderas@gmail.com>
2022-07-05 23:00:33 -0300

    Se creo ActivityHome con viewPager2

--- app/src/main/java/com/leandroid/system/mygymroutines/ui/view/home/HomeActivity.kt
+++ app/src/main/java/com/leandroid/system/mygymroutines/ui/view/home/HomeActivity.kt
@@ -2,15 +2,28 @@ package com.leandroid.system.mygymroutines.ui.view.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.leandroid.system.mygymroutines.R
+import com.google.android.material.tabs.TabLayoutMediator
+import com.leandroid.system.mygymroutines.databinding.ActivityHomeBinding
+import com.leandroid.system.mygymroutines.ui.view.days.ViewPagerAdapter
 
-enum class ProviderType{
-    BASIC
-}
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    var tabTitle = arrayOf("LUN","MAR","MIE","JUE","VIE","SAB")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        var pager = binding.viewPager2
+        var tl = binding.tabLayout
+        pager.adapter = ViewPagerAdapter(supportFragmentManager,lifecycle)
+
+        TabLayoutMediator(tl, pager){
+            tab, position ->
+                tab.text = tabTitle[position]
+        }.attach()
     }
 }
